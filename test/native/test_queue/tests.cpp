c6a5114864be82ad15b95b7900c11c6079fd8af8
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-05-04 09:33:32 +0200

    Add tests for the queue

--- test/native/test_queue/tests.cpp
+++ test/native/test_queue/tests.cpp
@@ -1,23 +1,94 @@
 #include <unity.h>
+#include "uint8-queue.h"
+
+uint8_queue_t q;
 
 void setUp(void)
 {
+    q = {0};
+    init_queue(&q);
 }
 
 void tearDown(void)
 {
 }
 
-void test_circular_buffer_empty_after_init()
+void test_is_empty_after_init()
 {
-    TEST_ASSERT_TRUE(true);
+    uint8_queue_t q2;
+    init_queue(&q2);
+    TEST_ASSERT_TRUE(queue_is_empty(&q2));
+    TEST_ASSERT_FALSE(queue_is_full(&q));
+}
+
+void test_can_take_an_item()
+{
+    add_to_queue(&q, 0);
+    TEST_ASSERT_FALSE(queue_is_empty(&q));
+    TEST_ASSERT_FALSE(queue_is_full(&q));
+}
+
+void test_can_take_max_items()
+{
+    int queue_size = 0;
+    while (!queue_is_full(&q))
+    {
+        add_to_queue(&q, 0);
+        queue_size++;
+    }
+    TEST_ASSERT_EQUAL(QUEUE_SIZE, queue_size);
+}
+
+void make_queue_full(uint8_queue_t *q)
+{
+    while (!queue_is_full(q))
+    {
+        add_to_queue(q, 0);
+    }
+}
+
+void test_can_remove_items()
+{
+    make_queue_full(&q);
+    dequeue(&q);
+    TEST_ASSERT_FALSE(queue_is_full(&q));
+}
+
+void test_empty_shall_return_invalid_dequeue()
+{
+    TEST_ASSERT_TRUE(queue_is_empty(&q));
+    dequeue_return_t v = dequeue(&q);
+    TEST_ASSERT_FALSE(v.is_valid);
+}
+
+void test_value_can_be_retrieved_from_queue()
+{
+    add_to_queue(&q, 0xaa);
+    dequeue_return_t v = dequeue(&q);
+    TEST_ASSERT_TRUE(v.is_valid);
+    TEST_ASSERT_EQUAL(0xaa, v.value);
+}
+
+void test_values_are_returned_in_fifo_order()
+{
+    add_to_queue(&q, 1);
+    add_to_queue(&q, 2);
+    dequeue_return_t v = dequeue(&q);
+    TEST_ASSERT_TRUE(v.is_valid);
+    TEST_ASSERT_EQUAL(1, v.value);
 }
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_circular_buffer_empty_after_init);
+    RUN_TEST(test_is_empty_after_init);
+    RUN_TEST(test_can_take_an_item);
+    RUN_TEST(test_can_take_max_items);
+    RUN_TEST(test_can_remove_items);
+    RUN_TEST(test_empty_shall_return_invalid_dequeue);
+    RUN_TEST(test_value_can_be_retrieved_from_queue);
+    RUN_TEST(test_values_are_returned_in_fifo_order);
 
     UNITY_END();
 }
