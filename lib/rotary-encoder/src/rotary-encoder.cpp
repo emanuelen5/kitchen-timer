bf7b482a7de0c76e32525ff9b318b16fd9904a74
Nicolas Perozzi <perozzi.nicolas@gmail.com>
2024-05-05 12:10:22 +0200

    added push button
    I added the push button using pin-change interrupt. By using pin-change interrupt, I avoid using INT1 which we can use for something else.

--- lib/rotary-encoder/src/rotary-encoder.cpp
+++ lib/rotary-encoder/src/rotary-encoder.cpp
@@ -7,8 +7,9 @@
 
 static event_cb_t cw_rotation;
 static event_cb_t ccw_rotation;
+static event_cb_t button_press;
 
-void init_rotary_encoder(event_cb_t cw_rotation_cb, event_cb_t ccw_rotation_cb)
+void init_rotary_encoder(event_cb_t cw_rotation_cb, event_cb_t ccw_rotation_cb, event_cb_t button_press_cb)
 {
     DDRD &= 0;
     PORTD |= bit(SW_PIN) | bit(CLK_PIN) | bit(DT_PIN);
@@ -16,8 +17,12 @@ void init_rotary_encoder(event_cb_t cw_rotation_cb, event_cb_t ccw_rotation_cb)
     cli();
     EIMSK |= bit(INT0);                // Interrupt enable INT0
     EICRA |= ~bit(ISC01) | bit(ISC00); // any change interrupt on INT0
+
+    PCICR |= bit(PCIE2);               // Enable Pin Change Interrupt for pin bank D
+    PCMSK2 |= bit(SW_PIN);             // Set mask to look for SW_PIN
     sei();
 
+    button_press = button_press_cb;
     cw_rotation = cw_rotation_cb;
     ccw_rotation = ccw_rotation_cb;
 }
@@ -37,4 +42,17 @@ ISR(INT0_vect)
             cw_rotation();
     }
     last_trigger_INT0 = t;
+}
+
+volatile unsigned long last_trigger_PCINT0 = 0;
+ISR(PCINT2_vect)
+{
+    unsigned long t = millis();
+    if (t - last_trigger_PCINT0)
+    {
+        if (bit_is_set(PIND, SW_PIN))
+        {
+            button_press();
+        }
+    }
 }
--- lib/rotary-encoder/src/rotary-encoder.h
+++ lib/rotary-encoder/src/rotary-encoder.h
@@ -3,11 +3,11 @@
 
 #define CLK_PIN PD2 // INT0
 #define DT_PIN PD3
-#define SW_PIN PD4
+#define SW_PIN PD4  //PCINT0
 
 //Funtion pointer declaration
 typedef void (*event_cb_t)(void);
 
-void init_rotary_encoder(event_cb_t cw_rot_ptr, event_cb_t ccw_rot_ptr);
+void init_rotary_encoder(event_cb_t cw_rot_ptr, event_cb_t ccw_rot_ptr, event_cb_t button_press_cb);
 
 #endif // ROTARY_ENCODER_H
