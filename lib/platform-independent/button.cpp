d9127a85b39939a3a26b3116c58b704c3ba8f4d8
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-07-16 22:03:18 +0200

    Make button into a class
    This will eventually solve #44

---
+++ lib/platform-independent/button.cpp
@@ -0,0 +1,69 @@
+#include "button.h"
+
+// If we include "millis.h" the LDF will include all of the atmega328 libraries,
+// which won't compile on the native host. So instead we just declare that the
+// function should exist at link time.
+extern uint16_t millis(void);
+
+Button::Button()
+    : last_press_time(0), press_count(0), is_pressed(false) {}
+
+void Button::press()
+{
+    is_pressed = true;
+    press_count++;
+
+    uint16_t now = millis();
+    uint16_t press_interval = now - last_press_time;
+
+    bool is_double_press = press_count == 2 && press_interval <= double_press_timeout_ms;
+    if (is_double_press)
+    {
+        on_double_press();
+        press_count = 0;
+    }
+
+    last_press_time = now;
+}
+
+void Button::release()
+{
+    if (!is_pressed)
+        return;
+
+    is_pressed = false;
+    uint16_t now = millis();
+    uint16_t press_duration = now - last_press_time;
+
+    if (press_duration > long_press_threshold_ms)
+    {
+        on_long_press();
+        press_count = 0;
+    }
+}
+
+/* Assumptions:
+ * - must be called every ms;
+ * - needs exclusive access (on AVR => `cli()` before being called)
+ */
+void Button::service()
+{
+    if (is_pressed)
+        return;
+
+    uint16_t now = millis();
+    uint16_t ms_since_press = now - last_press_time;
+    if (ms_since_press <= double_press_timeout_ms)
+        return;
+
+    if (press_count == 1)
+    {
+        on_single_press();
+        press_count = 0;
+    }
+    else if (press_count == 2)
+    {
+        on_double_press();
+        press_count = 0;
+    }
+}
---
+++ test/native/test_button/tests.cpp
@@ -0,0 +1,152 @@
+#include <unity.h>
+#include "button.h"
+
+Button *btn;
+class TestState
+{
+public:
+    TestState()
+        : time_ms(0), single_presses(0), double_presses(0), long_presses(0){};
+
+    uint16_t time_ms;
+    int single_presses, double_presses, long_presses;
+
+    void increment_time(uint16_t ms)
+    {
+        set_time(time_ms + ms);
+    }
+
+    void set_time(uint16_t ms)
+    {
+        TEST_ASSERT_TRUE_MESSAGE(ms > time_ms, "Cannot go back in time");
+        while (time_ms < ms)
+        {
+            time_ms++;
+            // We have to call the service routine every millisecond to
+            // guarantee operation
+            btn->service();
+        }
+    }
+};
+
+TestState *state;
+
+void setUp(void)
+{
+    state = new TestState();
+    btn = new Button();
+}
+
+uint16_t millis(void)
+{
+    return state->time_ms;
+}
+
+void tearDown(void)
+{
+    delete state;
+    delete btn;
+}
+
+void Button::on_single_press()
+{
+    state->single_presses++;
+}
+
+void Button::on_double_press()
+{
+    state->double_presses++;
+}
+
+void Button::on_long_press()
+{
+    state->long_presses++;
+}
+
+#define TEST_ASSERT_BUTTON_STATE(singles, doubles, longs) \
+    assert_button_state(singles, doubles, longs, __LINE__)
+
+void assert_button_state(int single_press, int double_press, int long_press, uint32_t line)
+{
+    UNITY_TEST_ASSERT_EQUAL_INT(single_press, state->single_presses, line, "Single press count");
+    UNITY_TEST_ASSERT_EQUAL_INT(double_press, state->double_presses, line, "Double press count");
+    UNITY_TEST_ASSERT_EQUAL_INT(long_press, state->long_presses, line, "Long press count");
+}
+
+void test_single_press(void)
+{
+    btn->press();
+    state->increment_time(1);
+    btn->release();
+
+    state->set_time(Button::double_press_timeout_ms);
+    TEST_ASSERT_BUTTON_STATE(false, false, false);
+
+    state->set_time(Button::double_press_timeout_ms + 1);
+    TEST_ASSERT_BUTTON_STATE(1, false, false);
+}
+
+void test_double_press(void)
+{
+    btn->press();
+    state->set_time(10);
+    btn->release();
+    state->set_time(20);
+    TEST_ASSERT_BUTTON_STATE(false, false, false);
+
+    btn->press();
+    TEST_ASSERT_BUTTON_STATE(false, true, false);
+}
+
+void test_long_press(void)
+{
+    btn->press();
+    state->increment_time(Button::long_press_threshold_ms + 1);
+    TEST_ASSERT_BUTTON_STATE(false, false, false);
+
+    btn->release();
+
+    TEST_ASSERT_BUTTON_STATE(false, false, 1);
+}
+
+void test_single_press_too_slow_for_double(void)
+{
+    btn->press();
+    state->set_time(Button::double_press_timeout_ms - 1);
+    btn->release();
+
+    state->set_time(Button::double_press_timeout_ms + 1);
+    TEST_ASSERT_BUTTON_STATE(1, false, false);
+}
+
+void test_press_twice(void)
+{
+    btn->press();
+    state->increment_time(Button::double_press_timeout_ms / 2 + 1);
+    btn->release();
+    state->increment_time(Button::double_press_timeout_ms / 2 + 1);
+
+    TEST_ASSERT_BUTTON_STATE(1, false, false);
+
+    btn->press();
+    state->increment_time(Button::double_press_timeout_ms / 2 + 1);
+    btn->release();
+    state->increment_time(Button::double_press_timeout_ms / 2 + 1);
+
+    TEST_ASSERT_BUTTON_STATE(2, false, false);
+}
+
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_single_press);
+    RUN_TEST(test_double_press);
+    RUN_TEST(test_long_press);
+    RUN_TEST(test_single_press_too_slow_for_double);
+    RUN_TEST(test_press_twice);
+
+    UNITY_END();
+
+    return 0;
+}
