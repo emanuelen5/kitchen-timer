f7e52b949db1481c5924ba74065ff29c0f306246
Nicolas Perozzi <perozzi.nicolas@gmail.com>
2024-07-23 08:45:01 +0200

    Fixing mistakes: I was overwriting SPCR over and over when I should have been setting the registers all at the same time.

--- lib/atmega328/SPI.cpp
+++ lib/atmega328/SPI.cpp
@@ -3,13 +3,10 @@
 #define CLK_PIN PB5
 #define DATA_PIN PB3
 
-
 void init_SPI(uint8_t cs_pin) {
     DDRB |= (1 << PB3) | (1 << PB5) | (1 << cs_pin); // Set MOSI, SCK, and CS as output
 
-    SPCR = (1 << SPE); //Enable SPI
-    SPCR = (1 << MSTR); //Set as Master
-    SPCR = (1 << SPR0); //Prescaler: Oscillator Frequency/16
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1); //Enable SPI, Set as Master, Prescaler: Oscillator Frequency/64
 
     PORTB |= (1 << cs_pin); //start with slave pin not selected
 }
