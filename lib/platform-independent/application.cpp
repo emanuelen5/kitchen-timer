68334ff1847745c63f99f073b05905e078b049e9
Nicolas Perozzi <perozzi.nicolas@gmail.com>
2024-08-17 00:06:04 +0200

    Modified step_application by moving the conditions for the event to the setp_application() function. This should make the code more readable

--- lib/platform-independent/application.cpp
+++ lib/platform-independent/application.cpp
@@ -9,6 +9,7 @@ static void debug_display(application_t *app);
 void init_application(application_t *app)
 {
     app->active_state_machine_index = 0;
+
     for (int8_t i = 0; i < MAX_TIMERS; i++)
     {
         init_state_machine(&app->state_machines[i]);
@@ -16,6 +17,28 @@ void init_application(application_t *app)
 }
 
 void step_application(application_t *app, event_t event)
+{
+    if(event == DOUBLE_PRESS)
+    {
+        select_next_state_machine(app);
+        if(app->active_state_machine_index >= MAX_TIMERS)
+        {
+            app->active_state_machine_index = 0;
+        }
+    }
+
+    else if(event == SECOND_TICK)
+    {
+        pass_event_to_all_state_machines(app, event);
+    }
+
+    else
+    {
+        step_state(&app->state_machines[app->active_state_machine_index], event);
+    }
+
+    debug_display(app);
+}
 
 static void select_next_state_machine(application_t *app)
 {
@@ -45,36 +68,4 @@ static void debug_display(application_t *app)
 void service_application(application_t *app)
 {
     service_state_machine(&app->state_machines[app->active_state_machine_index]);
-}
-
-
-void handle_second_tick(application_t *app, event_t event)
-{
-    if(event == SECOND_TICK)
-    {
-        for (int8_t i = 0; i < MAX_TIMERS; i++)
-        {
-            if(get_state(&app->state_machines[i]) == RUNNING)
-            {
-                increment_sm_current_timer(&app->state_machines[i]);
-                if(get_state(&app->state_machines[i]) == RINGING)
-                {
-                    app->active_state_machine_index = i;
-                    UART_printf("T%d: Alarm goes off!!!\n", app->active_state_machine_index);
-                }
-            }
-        }
-    }
-}
-
-void handle_sm_change(application_t *app, event_t event)
-{
-    if(event == DOUBLE_PRESS)
-    {
-        app->active_state_machine_index++;
-        if(app->active_state_machine_index >= MAX_TIMERS)
-        {
-            app->active_state_machine_index = 0;
-        }
-    }
 }
