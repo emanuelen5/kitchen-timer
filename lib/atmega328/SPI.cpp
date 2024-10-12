b252007645cbfcd2ad66e4b350e88073d558b905
Nicolas Perozzi <perozzi.nicolas@gmail.com>
2024-10-12 09:03:30 +0200

    Created the ISR and enabled the SPI interrupt. Additionally, I declared the spi_cmds variable and deactivate_cs(void) as extern cause I needed them for the ISR but, since SPI and max72xx have a circular dependency, they were generating multiple definitions.

--- lib/atmega328/SPI.cpp
+++ lib/atmega328/SPI.cpp
@@ -1,20 +1,46 @@
 #include "SPI.h"
+#include <avr/interrupt.h>
 #include "util.h"
+#include "max72xx.h"
 
 #define CLK_PIN PB5
 #define DATA_PIN PB3
 
-void init_SPI(uint8_t cs_pin) {
+extern SPI_context_t spi_cmds;
+extern void deactivate_cs(void);
+
+void init_SPI(uint8_t cs_pin)
+{
     DDRB |= bit(PB3) | bit(PB5) | bit(cs_pin); // Set MOSI, SCK, and CS as output
 
-    SPCR = bit(SPE) | bit(MSTR) | bit(SPR1); //Enable SPI, Set as Master, Prescaler: Oscillator Frequency/16.
+    SPCR = bit(SPE) | bit(MSTR) | bit(SPR1) | bit(SPIE); //Enable SPI, Set as Master, Prescaler: Oscillator Frequency/16, enable SPI interrupt
     //TODO: Adjust the prescaler once we get the real PCB.
 
     PORTB |= bit(cs_pin); //start with slave pin not selected
 }
 
+ISR(SPI_STC_vect)
+{
+    spi_cmds.index++;
+
+    if (spi_cmds.index < spi_cmds.length)
+    {
+        if (spi_cmds.index % 2 == 0)
+        {
+            SPDR = spi_cmds.cmds[spi_cmds.index / 2].reg; 
+        }
+        else
+        {
+            SPDR = spi_cmds.cmds[spi_cmds.index / 2].data; 
+        }
+    }
+    else
+    {
+        deactivate_cs();
+    }
+}
+
 void SPI_transmit_byte(uint8_t byte)
 {
     SPDR = byte;
-    while (!(SPSR & bit(SPIF)));
 }
