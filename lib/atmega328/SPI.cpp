be1d0cf91dead730a1f55576278d1d999b3811b2
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-10-21 13:05:10 +0200

    Keep SPI block in master mode
    This change fixes a bug where the SPI HW module switches from master
    mode whenever the SS pin went low.

--- lib/atmega328/SPI.cpp
+++ lib/atmega328/SPI.cpp
@@ -3,7 +3,11 @@
 
 void init_SPI(void)
 {
-    DDRB |= bit(DATA_PIN) | bit(CLK_PIN) | bit(CS_PIN);
+    // The SS pin must be set as an output, otherwise the SPI HW block will
+    // switch from Master to slave mode whenever SS is driven low. Source: SS
+    // Pin Functionality, ch. 19.3, megaAVR data sheet, Micro Chip, 2020.
+    const uint8_t ss_pin = PB2;
+    DDRB |= bit(DATA_PIN) | bit(CLK_PIN) | bit(ss_pin) | bit(CS_PIN);
     deactivate_cs();
 
     SPCR = bit(SPE) | bit(MSTR) | bit(SPR1); // Enable SPI, Set as Master, Prescaler: Oscillator Frequency/16.
