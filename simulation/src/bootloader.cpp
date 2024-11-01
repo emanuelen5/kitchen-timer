32625e194ce6e1619b76cce06769af7ea9a654ed
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-11-01 12:46:51 +0100

    Use getopts to parse arguments
    Break out argument parsing into a function

--- simulation/src/bootloader.cpp
+++ simulation/src/bootloader.cpp
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <getopt.h>
+#include <string>
+#include <cerrno>
+#include <iostream>
+#include <stdexcept>
 
 extern "C"
 {
@@ -16,7 +21,7 @@ uart_pty_t uart_pty;
 
 struct avr_flash
 {
-    char avr_flash_path[1024];
+    std::string avr_flash_path;
     int avr_flash_fd;
 };
 
@@ -28,20 +33,20 @@ void avr_special_init(avr_t *avr, void *data)
 
     printf("%s\n", __func__);
     // open the file
-    flash_data->avr_flash_fd = open(flash_data->avr_flash_path,
+    flash_data->avr_flash_fd = open(flash_data->avr_flash_path.c_str(),
                                     O_RDWR | O_CREAT, 0644);
     if (flash_data->avr_flash_fd < 0)
     {
-        perror(flash_data->avr_flash_path);
+        perror(flash_data->avr_flash_path.c_str());
         exit(1);
     }
-    // resize and map the file the file
+    // resize and map the file
     (void)ftruncate(flash_data->avr_flash_fd, avr->flashend + 1);
     ssize_t r = read(flash_data->avr_flash_fd, avr->flash, avr->flashend + 1);
     if (r != avr->flashend + 1)
     {
         fprintf(stderr, "unable to load flash memory\n");
-        perror(flash_data->avr_flash_path);
+        perror(flash_data->avr_flash_path.c_str());
         exit(1);
     }
 }
@@ -57,8 +62,8 @@ void avr_special_deinit(avr_t *avr, void *data)
     ssize_t r = write(flash_data->avr_flash_fd, avr->flash, avr->flashend + 1);
     if (r != avr->flashend + 1)
     {
-        fprintf(stderr, "unable to load flash memory\n");
-        perror(flash_data->avr_flash_path);
+        fprintf(stderr, "unable to flush flash memory\n");
+        perror(flash_data->avr_flash_path.c_str());
     }
     close(flash_data->avr_flash_fd);
     uart_pty_stop(&uart_pty);
@@ -81,40 +86,104 @@ bool endswith(const char *s, const char *suffix)
     return strcmp(s + strlen(s) - strlen(suffix), suffix) == 0;
 }
 
-int main(int argc, char *argv[])
+typedef struct args
 {
-    struct avr_flash flash_data;
-    char boot_path[1024] = "ATmegaBOOT_168_atmega328.ihex";
-    uint32_t boot_base, boot_size;
-    const char *mmcu = "atmega328p";
-    const uint32_t freq = 1000000;
-    int debug = 0;
-    int verbose = 0;
+    bool debug;
+    bool verbose;
+    std::string hex_file;
+} args_t;
 
-    for (int i = 1; i < argc; i++)
+class ArgumentError : public std::runtime_error
+{
+public:
+    explicit ArgumentError(const std::string &message)
+        : std::runtime_error(message) {}
+};
+
+void print_usage(std::string program_name)
+{
+    std::cerr << "Usage: " + program_name + " [-d|--debug] [-v|--verbose] hex_file";
+}
+
+args_t
+parse_arguments(int argc, char *argv[])
+{
+    const struct option long_options[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {"debug", no_argument, nullptr, 'd'},
+        {"verbose", no_argument, nullptr, 'v'},
+        {nullptr, 0, nullptr, 0}};
+
+    args_t args = {.debug = false, .verbose = false, .hex_file = ""};
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "hdvp:", long_options, nullptr)) != -1)
     {
-        if (endswith(argv[i], ".hex"))
-            strncpy(boot_path, argv[i], sizeof(boot_path));
-        else if (!strcmp(argv[i], "-d"))
-            debug++;
-        else if (!strcmp(argv[i], "-v"))
-            verbose++;
-        else
+        switch (opt)
         {
-            fprintf(stderr, "%s: invalid argument %s\n", argv[0], argv[i]);
-            exit(1);
+        case 'd':
+            args.debug = true;
+            break;
+        case 'v':
+            args.verbose = true;
+            break;
+        case 'h':
+        {
+            const std::string program_name = std::string(argv[0]);
+            print_usage(program_name);
+            exit(0);
+            break;
+        }
+        default:
+            throw ArgumentError("Unknown flag '" + std::to_string(opt) + "'");
+            break;
         }
     }
 
-    uint8_t *boot = read_ihex_file(boot_path, &boot_size, &boot_base);
+    int arguments_left = argc - optind;
+
+    if (arguments_left <= 0)
+        throw ArgumentError("hex_file argument is required.");
+    if (arguments_left > 1)
+        throw ArgumentError("Too many arguments.");
+
+    if (!endswith(argv[optind], ".hex"))
+        throw ArgumentError("hex_file isn't a .hex file");
+
+    args.hex_file = argv[optind];
+
+    return args;
+}
+
+int main(int argc, char *argv[])
+{
+    args_t args;
+    try
+    {
+        args = parse_arguments(argc, argv);
+    }
+    catch (const ArgumentError &e)
+    {
+        const std::string program_name = std::string(argv[0]);
+        std::cerr << "Argument error: " << e.what() << "\n";
+        print_usage(program_name);
+        return 1;
+    }
+
+    struct avr_flash flash_data;
+    uint32_t boot_base, boot_size;
+    std::string mmcu = "atmega328p";
+    const uint32_t freq = 1000000;
+
+    uint8_t *boot = read_ihex_file(args.hex_file.c_str(), &boot_size, &boot_base);
     if (!boot)
     {
-        fprintf(stderr, "%s: Unable to load %s\n", argv[0], boot_path);
+        fprintf(stderr, "%s: Unable to load %s\n", argv[0], args.hex_file.c_str());
         exit(1);
     }
-    printf("%s bootloader 0x%05x: %d bytes\n", mmcu, boot_base, boot_size);
+    printf("%s bootloader 0x%05x: %d bytes\n", mmcu.c_str(), boot_base, boot_size);
 
-    avr_t *avr = avr_make_mcu_by_name(mmcu);
+    avr_t *avr = avr_make_mcu_by_name(mmcu.c_str());
     if (!avr)
     {
         fprintf(stderr, "%s: Error creating the AVR core\n", argv[0]);
@@ -129,8 +198,7 @@ int main(int argc, char *argv[])
             .efuse = 0xFF,
         });
 
-    snprintf(flash_data.avr_flash_path, sizeof(flash_data.avr_flash_path),
-             "simduino_%s_flash.bin", mmcu);
+    flash_data.avr_flash_path = mmcu + ".flash.bin";
     flash_data.avr_flash_fd = 0;
     // register our own functions
     avr->custom.init = avr_special_init;
@@ -144,11 +212,11 @@ int main(int argc, char *argv[])
     avr->pc = boot_base;
     /* end of flash, remember we are writing /code/ */
     avr->codeend = avr->flashend;
-    avr->log = 1 + verbose;
+    avr->log = 1 + args.verbose;
 
     // even if not setup at startup, activate gdb if crashing
     avr->gdb_port = 1234;
-    if (debug)
+    if (args.debug)
     {
         avr->state = cpu_Stopped;
         avr_gdb_init(avr);
