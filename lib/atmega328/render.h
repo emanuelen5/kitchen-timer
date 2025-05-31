766b14c74e550e4d5dee7cedb02decdecfc7b63c
nperozzi <perozzinicolas@gmail.com>
2025-05-31 15:24:03 +0200

    I started the creation of a render.h library

---
+++ lib/atmega328/render.h
@@ -0,0 +1,15 @@
+//Converts total_seconds to minutes/seconds or hours/minutes based on value
+//Clears the matrix buffer
+//Uses the fatfont_get_char(char) function to get 7x6 bitmaps
+//Uses matrix_set_pixel(x, y, is_on) to draw digits
+//Calls matrix_update() only if something changed
+
+#ifndef RENDER_H
+#define RENDER_H
+
+#include <stdint.h>
+#include "state-machine.h"
+
+void render_timer_view(state_machine_t* timers, uint8_t timer_count, uint8_t active_index, uint32_t millis_now);
+
+#endif // RENDER_H
