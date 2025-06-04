591af1a15b511672d70712643ab951d1e0c32ab5
nperozzi <perozzinicolas@gmail.com>
2025-06-04 11:08:20 +0200

    Fixed display flickering and crashing after a few seconds

--- lib/atmega328/render.h
+++ lib/atmega328/render.h
@@ -6,6 +6,7 @@
 #include "state-machine.h"
 #include "fat_font.h"
 #include "max72xx_matrix.h"
+#include "millis.h"
 
 void init_render();
 void render_timer_view(state_machine_t* timers, uint8_t timer_count, uint8_t active_timer_index);
