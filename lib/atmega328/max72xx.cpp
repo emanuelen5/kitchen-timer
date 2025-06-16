4bcd82ace9f1afd4d7c878930142ede953e7add3
nperozzi <perozzinicolas@gmail.com>
2025-06-16 11:00:21 +0200

    mode the brightness setting to config.h

--- lib/atmega328/max72xx.cpp
+++ lib/atmega328/max72xx.cpp
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include "util.h"
 #include "SPI.h"
+#include "config.h"
 
 void max72xx_send_commands(max72xx_cmd_t *cmds, uint8_t length);
 void max72xx_send_commands_to_all(max72xx_reg_t reg, uint8_t data);
@@ -13,7 +14,7 @@ void init_max72xx(void)
     max72xx_send_commands_to_all(Max72XX_Shutdown, 0x01);     // normal operation (exit shutdown mode)
     max72xx_send_commands_to_all(Max72XX_Scan_Limit, 0x07);   // 8 digits scan limit
     max72xx_send_commands_to_all(Max72XX_Decode_Mode, 0x00);  // disable decode mode
-    max72xx_send_commands_to_all(Max72XX_Intensity, 0x00);    // set brightness
+    max72xx_send_commands_to_all(Max72XX_Intensity, DISPLAY_BRIGHTNESS);    // set brightness
     max72xx_send_commands_to_all(Max72XX_Display_Test, 0x00); // disable display test
 }
 
--- lib/platform-independent/config.h
+++ lib/platform-independent/config.h
@@ -2,5 +2,6 @@
 #define CONFIG_H
 
 #define MAX_TIMERS 16
+#define DISPLAY_BRIGHTNESS 0x0a //from 0x00 to 0x0f
 
 #endif // CONFIG_H
