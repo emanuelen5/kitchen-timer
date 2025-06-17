d6089e3c5415dae35a9162287b1fd774f9e3a377
nperozzi <perozzinicolas@gmail.com>
2025-06-17 22:54:35 +0200

    Modified main to work with the application library instead of using just the state_machine library.

--- src/envs/render_timer_view/main.cpp
+++ src/envs/render_timer_view/main.cpp
@@ -7,7 +7,7 @@
 #include "UART.h"
 
 #include "render.h"
-#include "led-counter.h"
+#include "application.h"
 
 uint8_queue_t eventQueue;
 static const uint8_t queue_buffer_size = 8;
@@ -15,6 +15,7 @@ uint8_t event_queue_buffer[queue_buffer_size];
 bool is_paused = false;
 
 state_machine_t sm;
+application_t app;
 
 void second_tick_cb(void)
 {
@@ -23,32 +24,29 @@ void second_tick_cb(void)
 
 int main()
 {
-    init_led_counter();
     init_UART();
-
     init_queue(&eventQueue, event_queue_buffer, queue_buffer_size);
     init_timer2_to_1s_interrupt(&second_tick_cb);
-    init_state_machine(&sm);
+    init_application(&app);
     init_render();
-
     sei();
     
-    sm.timer.original_time = 10; //Test with 1 minute and 10 secs
-    step_state(&sm, SINGLE_PRESS); //To move into RUNNING state
+    app.state_machines[0].timer.original_time = 10; //Test with 1 minute and 10 secs
+    step_application(&app, SINGLE_PRESS); //To move into RUNNING state
 
     while (true)
     {
         uint16_t millis_now = millis();
 
-        if(!is_paused && millis_now >= 3001)
+        if(!is_paused && millis_now >= 3000)
         {
-            step_state(&sm, SINGLE_PRESS); // Simulate PAUSE
+            step_application(&app, SINGLE_PRESS); // Simulate PAUSE
             is_paused = true;
         }
 
-        if(is_paused && millis_now >= 7001)
+        if(is_paused && millis_now >= 7000)
         {
-            step_state(&sm, SINGLE_PRESS);
+            step_application(&app, SINGLE_PRESS);
             is_paused = false;
         }
 
@@ -56,11 +54,10 @@ int main()
         dequeue_return_t event = dequeue(&eventQueue);
         if (event.is_valid)
         {
-            step_state(&sm, (event_t)event.value);
-            //increment_counter();
+            step_application(&app, (event_t)event.value);
         }
-        service_state_machine(&sm);
-        render_timer_view(&sm, 5, 0);
+        service_application(&app);
+        render_timer_view(&app.state_machines[0], 5, 0);
     }
 
     return 0;
