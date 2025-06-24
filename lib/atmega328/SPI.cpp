fdabc0ca90a0db391ef3d35600d5eb20062dd68e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2025-06-24 20:08:46 +0200

    Don't enable interrupts in setup

--- lib/atmega328/SPI.cpp
+++ lib/atmega328/SPI.cpp
@@ -30,13 +30,11 @@ void init_SPI(uint8_t bytes)
 
     // Enable SPI, Master, set clock rate (fosc/16)
     SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR1);
-    SPSR |= (1 << SPI2X);  // Double SPI speed if necessary (for 1 MHz)
-    
+    SPSR |= (1 << SPI2X); // Double SPI speed if necessary (for 1 MHz)
+
     // Enable SPI interrupt
     SPCR |= (1 << SPIE);
 
-    sei();
-    
     message_length = bytes;
     init_queue(&SPI_queue, SPI_queue_buffer, SPI_queue_size);
 }
