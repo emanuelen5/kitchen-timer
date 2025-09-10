64625b2f51860a3dd3836ed34aa4969bf7139448
nperozzi <perozzinicolas@gmail.com>
2025-09-10 19:40:58 +0200

    drafted the handle_command() function and the serial_commands library.

---
+++ lib/platform-independent/serial_commands.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdint.h>
+
+typedef struct
+{
+    void (*led_on)(void);
+    void (*led_off)(void);
+} command_callbacks_t;
+
+void handle_command(const char* cmd, command_callbacks_t* callbacks);
+
+#endif
