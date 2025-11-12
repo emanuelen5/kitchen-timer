1a99b99d8aaad02ef14d84faf0e9655e594a4868
Nicolas Perozzi <perozzinicolas@gmail.com>
2025-11-12 20:11:44 +0100

    Changes on the unit test
    Simplified the tests and put all the testing variables into a struct to make it easier to reset between tests.

--- test/native/test_serial_commands/tests.cpp
+++ test/native/test_serial_commands/tests.cpp
@@ -1,41 +1,35 @@
 #include <unity.h>
 #include "serial_commands.h"
 #include "application.h"
+#include <string.h>
 
-static bool led_on_called;
-static bool version_called;
-static bool set_active_timer_called;
-static uint32_t last_timer_value;
-static bool play_active_timer_called;
-static bool pause_active_timer_called;
-static bool reset_active_timer_called;
-static bool get_status_active_timer_called;
-static bool setup_brightness_called;
-static uint8_t brightness_value;
-static bool setup_volume_called;
-static bool setup_status_called;
-static bool help_called;
-static bool unrecognized_command_called;
-static bool reboot_called;
+struct 
+{
+    bool led_on_called;
+    bool version_called;
+    bool set_active_timer_called;
+    uint32_t last_timer_value;
+    bool play_active_timer_called;
+    bool pause_active_timer_called;
+    bool reset_active_timer_called;
+    bool get_status_active_timer_called;
+    bool setup_brightness_called;
+    uint8_t brightness_value;
+    bool setup_volume_called;
+    bool setup_status_called;
+    bool help_called;
+    bool unrecognized_command_called;
+    char unrecognized_command_str[100];
+    bool reboot_called;
+
+} test_state;
+
 
 application_t app;
 
 void setUp(void)
 {
-    led_on_called = false;
-    version_called = false;
-    set_active_timer_called = false;
-    last_timer_value = 0;
-    play_active_timer_called = false;
-    pause_active_timer_called = false;
-    reset_active_timer_called = false;
-    get_status_active_timer_called = false;
-    setup_brightness_called = false;
-    setup_volume_called = false;
-    setup_status_called = false;
-    help_called = false;
-    unrecognized_command_called = false;
-    reboot_called = false;
+    test_state = {};
 }
 
 void tearDown(void) {}
@@ -60,104 +54,67 @@ void test_led(bool is_on)
 {
     if (is_on)
     {
-        led_on_called = true;
+        test_state.led_on_called = true;
     }
     else
     {
-        led_on_called = false;
+        test_state.led_on_called = false;
     }
 }
 
 void version(void)
 {
-    version_called = true;
+    test_state.version_called = true;
 }
 
 void set_active_timer(state_machine_t *active_sm, uint32_t *steps)
 { 
     set_state(active_sm, SET_TIME);
-    set_active_timer_called = true;
-    last_timer_value = *steps;
+    test_state.set_active_timer_called = true;
+    test_state.last_timer_value = *steps;
 }
 
 void play_active_timer(state_machine_t *active_sm)
 {
-    if(get_state(active_sm) == SET_TIME)
-    {
-        play_active_timer_called = true;
-    }
-    else
-    {
-        play_active_timer_called = false;
-    }
+    (void)active_sm;
+    test_state.play_active_timer_called = true;
 }
 
 void pause_active_timer(state_machine_t *active_sm)
 {
-    if(get_state(active_sm) == SET_TIME)
-    {
-        pause_active_timer_called = true;
-    }
-    else
-    {
-        pause_active_timer_called = false;
-    }
+    (void)active_sm;
+    test_state.pause_active_timer_called = true;
 }
 
 void reset_active_timer(state_machine_t *active_sm)
 {
-    if(get_state(active_sm) == SET_TIME)
-    {
-        reset_active_timer_called = true;
-    }
-    else
-    {
-        reset_active_timer_called = false;
-    }
+    (void)active_sm;
+    test_state.reset_active_timer_called = true;
 }
 
 void get_status_active_timer(state_machine_t *active_sm)
 {
-    if(get_state(active_sm) == SET_TIME)
-    {
-        get_status_active_timer_called = true;
-    }
-    else
-    {
-        get_status_active_timer_called = false;
-    }
+    (void)active_sm;
+    test_state.get_status_active_timer_called = true;
 }
 
 void setup_brightness(uint8_t *intensity)
 {
-    if(*intensity > 16)
-    {
-        setup_brightness_called = false;
-        return;
-    }
-    setup_brightness_called = true;
-    brightness_value = *intensity;
+    test_state.brightness_value = *intensity;
+    test_state.setup_brightness_called = true;
 }
 
 void setup_volume(Buzzer* buzzer, uint8_t *volume)
 {
-    if(*volume > 11)
-    {
-        setup_volume_called = false;
-        return;
-    }
-    setup_volume_called = true;
-    buzzer->set_volume(*volume);
+    (void)buzzer;
+    (void)volume;
+    test_state.setup_volume_called = true;
 }
 
 void setup_status(application_t *app)
 {
-    if(app->buzzer.get_volume() != 10)
-    {
-        setup_status_called = false;
-        return;
-    }
-    setup_status_called = true;
+    (void)app;
+    test_state.setup_status_called = true;
 }
 
 void setup_buzzer(Buzzer* buzzer, bool is_on)
@@ -179,21 +136,18 @@ void test_buzzer(Buzzer *buzzer)
 
 void help_cmd(void)
 {
-    help_called = true;
+    test_state.help_called = true;
 }
 
 void unrecognized_command(char *string)
 {
-    if(string == NULL)
-    {
-        return;
-    }
-    unrecognized_command_called = true;
+    test_state.unrecognized_command_called = true;
+    strncpy(test_state.unrecognized_command_str, string, sizeof(test_state.unrecognized_command_str));
 }
 
 void reboot(void)
 {
-    reboot_called = true;
+    test_state.reboot_called = true;
 }
 
 static const command_callbacks_t callbacks =
@@ -220,72 +174,72 @@ void test_test_led_on_command(void)
 {
     char cmd[] = "test led on";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(led_on_called);
+    TEST_ASSERT_TRUE(test_state.led_on_called);
 }
 
 void test_test_led_off_command(void)
 {
     char cmd[] = "test led off";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_FALSE(led_on_called);
+    TEST_ASSERT_FALSE(test_state.led_on_called);
 }
 
 void test_version_command(void)
 {
     char cmd[] = "version";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(version_called);
+    TEST_ASSERT_TRUE(test_state.version_called);
 }
 
 void test_timer_set_command(void)
 {
     char cmd[] = "timer set 01:02:03";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(set_active_timer_called);
-    TEST_ASSERT_EQUAL_UINT32(3723, last_timer_value); // 1*3600 + 2*60 + 3
+    TEST_ASSERT_TRUE(test_state.set_active_timer_called);
+    TEST_ASSERT_EQUAL_UINT32(3723, test_state.last_timer_value); // 1*3600 + 2*60 + 3
 }
 
 void test_timer_play_command(void)
 {
     char cmd[] = "timer play";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(play_active_timer_called);
+    TEST_ASSERT_TRUE(test_state.play_active_timer_called);
 }
 
 void test_timer_pause_command(void)
 {
     char cmd[] = "timer pause";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(pause_active_timer_called);
+    TEST_ASSERT_TRUE(test_state.pause_active_timer_called);
 }
 
 void test_timer_reset_command(void)
 {
     char cmd[] = "timer reset";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(reset_active_timer_called);
+    TEST_ASSERT_TRUE(test_state.reset_active_timer_called);
 }
 
 void test_timer_status_command(void)
 {
     char cmd[] = "timer status";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(get_status_active_timer_called);
+    TEST_ASSERT_TRUE(test_state.get_status_active_timer_called);
 }
 
 void test_setup_brightness_command(void)
 {
     char cmd[] = "setup brightness 10";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(setup_brightness_called);
-    TEST_ASSERT_EQUAL_UINT8(10, brightness_value);
+    TEST_ASSERT_TRUE(test_state.setup_brightness_called);
+    TEST_ASSERT_EQUAL_UINT8(10, test_state.brightness_value);
 }
 
 void test_setup_volume_command(void)
 {
     char cmd[] = "setup volume 10";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(setup_volume_called);
+    TEST_ASSERT_TRUE(test_state.setup_volume_called);
     TEST_ASSERT_EQUAL_UINT8(10, app.buzzer.get_volume());
 }
 
@@ -293,9 +247,7 @@ void test_setup_status_command(void)
 {
     char cmd[] = "setup status";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(setup_status_called);
-    TEST_ASSERT_EQUAL_UINT8(10, brightness_value);
-    TEST_ASSERT_EQUAL_UINT8(10, app.buzzer.get_volume());
+    TEST_ASSERT_TRUE(test_state.setup_status_called);
 }
 
 void test_setup_buzzer_command(void)
@@ -320,21 +272,23 @@ void test_help_command(void)
 {
     char cmd[] = "help";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(help_called);
+    TEST_ASSERT_TRUE(test_state.help_called);
 }
 
 void test_unrecognized_command(void)
 {
     char cmd[] = "asdf";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(unrecognized_command_called);
+    cmd[0] = '\0';
+    TEST_ASSERT_TRUE(test_state.unrecognized_command_called);
+    TEST_ASSERT_EQUAL_STRING("asdf", test_state.unrecognized_command_str);
 }
 
 void test_reboot_command(void)
 {
     char cmd[] = "reboot";
     handle_command(cmd, &callbacks, &app);
-    TEST_ASSERT_TRUE(reboot_called);
+    TEST_ASSERT_TRUE(test_state.reboot_called);
 }
 
 int main(void) {
