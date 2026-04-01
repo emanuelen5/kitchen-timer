af7b2d28b2893678ee66163cfe8b891a4638d554
Erasmus Cedernaes <github@emaus.dev>
2026-04-01 20:59:53 +0200

    Address review comment by Copilot

--- lib/platform-independent/state-machine.cpp
+++ lib/platform-independent/state-machine.cpp
@@ -27,7 +27,9 @@ bool state_machine_is_idle(state_machine_t *sm)
 void init_state_machine(state_machine_t *sm)
 {
     sm->prev_state = SET_TIME;
-    reset_active_state_machine(sm);
+    sm->state = SET_TIME;
+    sm->millis_of_last_transition = 0;
+    reset_timer(&sm->timer);
 }
 
 void service_state_machine(state_machine_t *sm)
