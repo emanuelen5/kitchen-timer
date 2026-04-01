496dbfb874c314d25c3692c0b0b278bc5db48c0a
Erasmus Cedernaes <github@emaus.dev>
2026-04-01 21:07:47 +0200

    Remove unused field

--- lib/platform-independent/state-machine.cpp
+++ lib/platform-independent/state-machine.cpp
@@ -9,7 +9,6 @@ uint16_t millis(void);
 void set_state(state_machine_t *sm, state_t new_state)
 {
     sm->millis_of_last_transition = millis();
-    sm->prev_state = sm->state;
     sm->state = new_state;
 }
 
@@ -26,7 +25,6 @@ bool state_machine_is_idle(state_machine_t *sm)
 
 void init_state_machine(state_machine_t *sm)
 {
-    sm->prev_state = SET_TIME;
     sm->state = SET_TIME;
     sm->millis_of_last_transition = 0;
     reset_timer(&sm->timer);
